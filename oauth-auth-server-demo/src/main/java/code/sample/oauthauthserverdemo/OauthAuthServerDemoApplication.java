0809e1d0be827a0c8a6e484d5106d5984c70ed3f
Sam Wong <samwong@Sam-M2.local>
2024-11-02 01:43:11 +0800

    init

---
+++ oauth-auth-server-demo/src/main/java/code/sample/oauthauthserverdemo/OauthAuthServerDemoApplication.java
@@ -0,0 +1,13 @@
+package code.sample.oauthauthserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthAuthServerDemoApplication.class, args);
+    }
+
+}
---
+++ oauth-auth-server-demo/src/main/java/code/sample/oauthauthserverdemo/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package code.sample.oauthauthserverdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        return http.formLogin().and().build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient =
+                RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("companyA_client")
+                        .clientSecret("{noop}companyA_secret")
+                        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                        .redirectUri("http://localhost:8080/login/oauth2/code/companyA-client-oidc")
+                        .scope(OidcScopes.OPENID)
+                        .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+}
